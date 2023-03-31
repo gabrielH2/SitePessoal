a2e6f2cf07e2af4f3eb13664f0f1e0addedbf11d
GABRIEL HENRIQUE DE ALBUQUERQUE MACHADO <125318539+gabrielH2@users.noreply.github.com>
2023-03-31 00:11:06 -0300

    Add files via upload

---
+++ ing.js
@@ -0,0 +1,28 @@
+var name = prompt('como vc se chama?')
+alert(' olá ' + name + ' é um prazer te ver aqui ')
+
+
+
+function idade(){
+     alert("Quantos vc acha que eu tenho? hahaha")
+}
+function nome(){
+    alert("Me chamo Gabriel Machado ")
+}
+function formacao(){
+    alert("Atualmente estou cursando o 3º periodo de sistemas para internet na Universidade católica de Pernambuco")
+}
+function vivo(){
+    alert("Moro em Recife, a maior capital em linha curva do mundo, hahaha")
+}
+function hob(){
+    alert("Pratico o ciclismo e tbm de ir a praia")
+}
+function Job(){
+    alert("trabalho na Universidade Católica na aréa de Suporte de TI")
+}
+function con(){
+    alert("gabrielmachado358@gmail.com")
+}
+
+
